e3f4a8bdc3ca5643ce88ecb2dfc5103119110914
Rodney Rodriguez <rodneyxr@gmail.com>
2017-03-14 17:49:42 -0500

    Added alias field to JSA's Nonterminal class for debugging

--- src/edu/utsa/fileflow/client/fileflow/variable/VariableAnalysis.java
+++ src/edu/utsa/fileflow/client/fileflow/variable/VariableAnalysis.java
@@ -19,183 +19,191 @@ import java.util.logging.Logger;
  */
 public class VariableAnalysis extends Analysis<VariableAnalysisDomain> {
 
-	private Logger logger = Logger.getLogger("VariableAnalysis");
+    private Logger logger = Logger.getLogger("VariableAnalysis");
 
-	@Override
-	public VariableAnalysisDomain onFinish(VariableAnalysisDomain domain) throws AnalysisException {
-		logger.log(Level.INFO, "\nLive Variables: {0}", domain.liveVariables);
-		logger.log(Level.INFO, "\nGrammar: {0}", domain.grammar);
-		return domain;
-	}
+    @Override
+    public VariableAnalysisDomain onFinish(VariableAnalysisDomain domain) throws AnalysisException {
+        logger.log(Level.INFO, "\nLive Variables: {0}", domain.liveVariables);
+        logger.log(Level.INFO, "\nGrammar: {0}", domain.grammar);
+        return domain;
+    }
 
-	@Override
-	public VariableAnalysisDomain onBefore(VariableAnalysisDomain domain, FlowPointContext context) throws AnalysisException {
+    @Override
+    public VariableAnalysisDomain onBefore(VariableAnalysisDomain domain, FlowPointContext context) throws AnalysisException {
 //		System.out.println("BEFORE");
 //		System.out.println(domain.liveVariables);
-		return super.onBefore(domain, context);
-	}
+        return super.onBefore(domain, context);
+    }
 
-	@Override
-	public VariableAnalysisDomain onAfter(VariableAnalysisDomain domain, FlowPointContext context) throws AnalysisException {
+    @Override
+    public VariableAnalysisDomain onAfter(VariableAnalysisDomain domain, FlowPointContext context) throws AnalysisException {
 //		System.out.println("AFTER");
 //		System.out.println(domain.liveVariables);
-		return super.onAfter(domain, context);
-	}
+        return super.onAfter(domain, context);
+    }
 
-	/**
-	 * Supported Operations:
-	 * var = var
-	 * var = var.var
-	 * var = '$literal'
-	 */
-	@Override
-	public VariableAnalysisDomain enterAssignment(VariableAnalysisDomain domain, FlowPointContext context) throws AnalysisException {
-		AssignContext ctx = new AssignContext(context);
-		int id = context.getFlowPoint().getID();
 
-		Variable v0 = new Variable(ctx.var0, id);
-		domain.grammar.addNonterminal(v0);
-		domain.liveVariables.addVariable(v0);
+    /**
+     * Supported Operations:
+     * var = var
+     * var = var.var
+     * var = '$literal'
+     */
+    @Override
+    public VariableAnalysisDomain enterAssignment(VariableAnalysisDomain domain, FlowPointContext context) throws AnalysisException {
+        AssignContext ctx = new AssignContext(context);
+        int id = context.getFlowPoint().getID();
 
-		// FIXME: this should be handled in the grammar
-		if (ctx.literal != null && (ctx.var1 != null || ctx.var2 != null)) {
-			throw new AnalysisException("literal and var cannot be both defined.");
-		}
+        Variable v0 = new Variable(ctx.var0, id);
+        domain.liveVariables.addVariable(v0);
 
-		// automaton production: $x0 = 'a';
-		if (ctx.literal != null) {
-			domain.grammar.addAutomatonProduction(v0, Automaton.makeString(ctx.literal));
-		} else if (ctx.var1 != null) {
-			// get or create v1
-			Set<Variable> v1Set = domain.liveVariables.getVariable(ctx.var1);
-			if (v1Set == null) {
-				throw new AnalysisException(String.format("%s is not defined", ctx.var1));
-			}
+        if (domain.grammar.visited.contains(id)) {
+            return domain;
+        }
 
-			for (Variable v1 : v1Set) {
-				if (ctx.var2 != null) {
-					// pair production: $x0 = $x1.$x2;
-					// get or create v2
-					Set<Variable> v2Set = domain.liveVariables.getVariable(ctx.var2);
-					if (v2Set == null) {
-						throw new AnalysisException(String.format("%s is not defined", ctx.var2));
-					}
-					for (Variable v2 : v2Set) {
-						domain.grammar.addPairProduction(v0, v1, v2);
-					}
-				} else {
-					// unit production: $x0 = $x1;
-					// DEBUG: System.out.println(String.format("%s -> %s", v0, v1));
-					domain.grammar.addUnitProduction(v0, v1);
-				}
-			}
-		}
+        domain.grammar.addNonterminal(v0);
+        System.out.println("After: " + domain.grammar.visited);
 
-		return domain;
-	}
+        // FIXME: this should be handled in the grammar
+        if (ctx.literal != null && (ctx.var1 != null || ctx.var2 != null)) {
+            throw new AnalysisException("literal and var cannot be both defined.");
+        }
 
-	class AssignContext {
-		final String var0;
-		final String var1;
-		final String var2;
-		final String literal;
-		final boolean isConcat;
-		final boolean isArray;
-		final boolean isEmptyArray;
-		final boolean isInput;
+        // automaton production: $x0 = 'a';
+        if (ctx.literal != null) {
+            domain.grammar.addAutomatonProduction(v0, Automaton.makeString(ctx.literal));
+        } else if (ctx.var1 != null) {
+            // get or create v1
+            Set<Variable> v1Set = domain.liveVariables.getVariable(ctx.var1);
+            if (v1Set == null) {
+                throw new AnalysisException(String.format("%s is not defined", ctx.var1));
+            }
 
-		public AssignContext(FlowPointContext fpctx) {
-			String $var0 = null;
-			String $var1 = null;
-			String $var2 = null;
-			String $literal = null;
-			boolean $isConcat = false;
-			boolean $isArray;
-			boolean $isEmptyArray;
-			boolean $isInput;
+            for (Variable v1 : v1Set) {
+                if (ctx.var2 != null) {
+                    // pair production: $x0 = $x1.$x2;
+                    // get or create v2
+                    Set<Variable> v2Set = domain.liveVariables.getVariable(ctx.var2);
+                    if (v2Set == null) {
+                        throw new AnalysisException(String.format("%s is not defined", ctx.var2));
+                    }
+                    for (Variable v2 : v2Set) {
+                        domain.grammar.addPairProduction(v0, v1, v2);
+                    }
+                } else {
+                    // unit production: $x0 = $x1;
+                    // DEBUG: System.out.println(String.format("%s -> %s", v0, v1));
+                    System.out.println(String.format("%s -> %s", v0, v1));
+                    domain.grammar.addUnitProduction(v0, v1);
+                }
+            }
+        }
 
-			AssignmentContext ctx = (AssignmentContext) fpctx.getContext();
-			VarValueContext var = ctx.varValue();
-			ArrayValueContext arr = ctx.arrayValue();
+        return domain;
+    }
 
-			// true if variable on left side is an array
-			$isArray = (ctx.Index() != null);
+    class AssignContext {
+        final String var0;
+        final String var1;
+        final String var2;
+        final String literal;
+        final boolean isConcat;
+        final boolean isArray;
+        final boolean isEmptyArray;
+        final boolean isInput;
 
-			ExpressionContext expr = null;
-			TerminalNode input = null;
-			TerminalNode emptyArray = null;
+        public AssignContext(FlowPointContext fpctx) {
+            String $var0 = null;
+            String $var1 = null;
+            String $var2 = null;
+            String $literal = null;
+            boolean $isConcat = false;
+            boolean $isArray;
+            boolean $isEmptyArray;
+            boolean $isInput;
 
-			// if assignment variable is array and has index
-			if ($isArray) {
-				if (arr.varValue() == null) {
-					emptyArray = arr.EmptyValue();
-				} else {
-					expr = arr.varValue().expression();
-					input = arr.varValue().Input();
-				}
-			} else {
-				// if assignment is a regular variable with no index
-				expr = var.expression();
-				input = var.Input();
-			}
+            AssignmentContext ctx = (AssignmentContext) fpctx.getContext();
+            VarValueContext var = ctx.varValue();
+            ArrayValueContext arr = ctx.arrayValue();
 
-			// variable name (key to update)
-			String key = ctx.Variable().getText();
-			if ($isArray)
-				key = key + ctx.Index().getText();
-			$var0 = key;
+            // true if variable on left side is an array
+            $isArray = (ctx.Index() != null);
 
-			// if array variable is assigned to empty array
-			$isEmptyArray = emptyArray != null;
+            ExpressionContext expr = null;
+            TerminalNode input = null;
+            TerminalNode emptyArray = null;
 
-			// if user input is required
-			$isInput = input != null;
+            // if assignment variable is array and has index
+            if ($isArray) {
+                if (arr.varValue() == null) {
+                    emptyArray = arr.EmptyValue();
+                } else {
+                    expr = arr.varValue().expression();
+                    input = arr.varValue().Input();
+                }
+            } else {
+                // if assignment is a regular variable with no index
+                expr = var.expression();
+                input = var.Input();
+            }
 
-			if (!$isEmptyArray && !$isInput) {
+            // variable name (key to update)
+            String key = ctx.Variable().getText();
+            if ($isArray)
+                key = key + ctx.Index().getText();
+            $var0 = key;
 
-				// get the first term
-				ValueContext term1 = expr.value(0);
-				if (term1.Variable() != null) {
-					// term1 is a variable
-					String text = term1.Variable().getText();
-					if (term1.Index() != null)
-						text += term1.Index().getText();
-					$var1 = text;
-				} else {
-					// term1 is a $literal
-					$literal = term1.String().getText();
-				}
+            // if array variable is assigned to empty array
+            $isEmptyArray = emptyArray != null;
 
-				// check for concatenation
-				if (expr.value().size() == 2) {
-					ValueContext term2 = expr.value(1);
-					if (term2.Variable() != null) {
-						// term2 is a variable
-						String text = term2.Variable().getText();
-						if (term2.Index() != null)
-							text += term2.Index().getText();
-						$var2 = text;
-					} else {
-						// term2 is a string
-						// this.$var2 = term2.String().getText();
-						System.err.println("VariableAnalysis Error: $var2 cannot be a $literal.");
-						System.exit(1);
-					}
+            // if user input is required
+            $isInput = input != null;
 
-					// concatenate $var1 and $var2
-					$isConcat = true;
-				}
-			}
+            if (!$isEmptyArray && !$isInput) {
 
-			this.var0 = $var0;
-			this.var1 = $var1;
-			this.var2 = $var2;
-			this.literal = $literal;
-			this.isConcat = $isConcat;
-			this.isArray = $isArray;
-			this.isEmptyArray = $isEmptyArray;
-			this.isInput = $isInput;
-		}
+                // get the first term
+                ValueContext term1 = expr.value(0);
+                if (term1.Variable() != null) {
+                    // term1 is a variable
+                    String text = term1.Variable().getText();
+                    if (term1.Index() != null)
+                        text += term1.Index().getText();
+                    $var1 = text;
+                } else {
+                    // term1 is a $literal
+                    $literal = term1.String().getText();
+                }
 
-	}
+                // check for concatenation
+                if (expr.value().size() == 2) {
+                    ValueContext term2 = expr.value(1);
+                    if (term2.Variable() != null) {
+                        // term2 is a variable
+                        String text = term2.Variable().getText();
+                        if (term2.Index() != null)
+                            text += term2.Index().getText();
+                        $var2 = text;
+                    } else {
+                        // term2 is a string
+                        // this.$var2 = term2.String().getText();
+                        System.err.println("VariableAnalysis Error: $var2 cannot be a $literal.");
+                        System.exit(1);
+                    }
+
+                    // concatenate $var1 and $var2
+                    $isConcat = true;
+                }
+            }
+
+            this.var0 = $var0;
+            this.var1 = $var1;
+            this.var2 = $var2;
+            this.literal = $literal;
+            this.isConcat = $isConcat;
+            this.isArray = $isArray;
+            this.isEmptyArray = $isEmptyArray;
+            this.isInput = $isInput;
+        }
+
+    }
 }
--- src/edu/utsa/fileflow/client/fileflow/variable/VariableGrammar.java
+++ src/edu/utsa/fileflow/client/fileflow/variable/VariableGrammar.java
@@ -20,117 +20,126 @@ import java.util.Set;
  */
 public class VariableGrammar implements Cloneable, Mergeable<VariableGrammar> {
 
-	private Grammar grammar = new Grammar();
+    private Grammar grammar = new Grammar();
 
-	// maps variables to nonterminals in the grammar
-	private Map<Variable, Nonterminal> variables = new HashMap<>();
-	private Set<Integer> inserted = new HashSet<>();
+    // maps variables to nonterminals in the grammar
+    private Map<Variable, Nonterminal> variables = new HashMap<>();
+    private Set<Integer> inserted = new HashSet<>();
+    public Set<Integer> visited = new HashSet<>();
 
-	/* Grammar to MLFA to Automaton */
-	private MLFA mlfa;
-	private MLFA2Automaton m2a;
-	private boolean isDirty = true; // true when the MLFA needs to be recomputed
+    /* Grammar to MLFA to Automaton */
+    private MLFA mlfa;
+    private MLFA2Automaton m2a;
+    private boolean isDirty = true; // true when the MLFA needs to be recomputed
 
-	Nonterminal addNonterminal(Variable v) {
-		isDirty = true;
-		Nonterminal nonterminal = grammar.addNonterminal();
-		variables.put(v, nonterminal);
-		return nonterminal;
-	}
+    Nonterminal addNonterminal(Variable v) {
+        isDirty = true;
+        visited.add(v.id);
+        Nonterminal nonterminal = grammar.addNonterminal(v.toString());
+        variables.put(v, nonterminal);
+        return nonterminal;
+    }
 
-	// $x0 = 'a';
-	public void addAutomatonProduction(Variable v, Automaton a) {
-		if (insert(v.id))
-			grammar.addAutomatonProduction(variables.get(v), a);
-	}
+    // $x0 = 'a';
+    public void addAutomatonProduction(Variable v, Automaton a) {
+        if (insert(v.id))
+            grammar.addAutomatonProduction(variables.get(v), a);
+    }
 
-	// $x0 = $x1;
-	public void addUnitProduction(Variable v0, Variable v1) {
-		if (insert(v0.id)) {
-			Nonterminal nt0 = variables.get(v0);
-			Nonterminal nt1 = variables.get(v1);
-			grammar.addUnitProduction(nt0, nt1);
-		}
-	}
+    // $x0 = $x1;
+    public void addUnitProduction(Variable v0, Variable v1) {
+        if (insert(v0.id)) {
+            Nonterminal nt0 = variables.get(v0);
+            Nonterminal nt1 = variables.get(v1);
+            grammar.addUnitProduction(nt0, nt1);
+        }
+    }
 
-	// $x0 = $x1.$x2;
-	public void addPairProduction(Variable v0, Variable v1, Variable v2) {
-		if (insert(v0.id)) {
-			Nonterminal nt0 = variables.get(v0);
-			Nonterminal nt1 = variables.get(v1);
-			Nonterminal nt2 = variables.get(v2);
-			grammar.addPairProduction(nt0, nt1, nt2);
-		}
-	}
+    // $x0 = $x1.$x2;
+    public void addPairProduction(Variable v0, Variable v1, Variable v2) {
+        if (insert(v0.id)) {
+            Nonterminal nt0 = variables.get(v0);
+            Nonterminal nt1 = variables.get(v1);
+            Nonterminal nt2 = variables.get(v2);
+            grammar.addPairProduction(nt0, nt1, nt2);
+        }
+    }
 
-	/**
-	 * Gets the variable from the grammar as an {@link Automaton}.
-	 *
-	 * @param variableSet The variable to get.
-	 * @return an {@link Automaton} representing the variable
-	 */
-	public Automaton getVariable(Set<Variable> variableSet) {
-		Automaton a = new Automaton();
-		Grammar2MLFA g2m = new Grammar2MLFA(grammar);
-		if (isDirty) {
-			mlfa = g2m.convert();
-			m2a = new MLFA2Automaton(mlfa);
-			isDirty = false;
-		}
+    /**
+     * Gets the variable from the grammar as an {@link Automaton}.
+     *
+     * @param variableSet The variable to get.
+     * @return an {@link Automaton} representing the variable
+     */
+    public Automaton getVariable(Set<Variable> variableSet) {
+        Automaton a = new Automaton();
+        Grammar2MLFA g2m = new Grammar2MLFA(grammar);
+        if (isDirty) {
+            mlfa = g2m.convert();
+            m2a = new MLFA2Automaton(mlfa);
+            isDirty = false;
+        }
 
-		for (Variable v : variableSet)
-			a = a.union(m2a.extract(g2m.getMLFAStatePair(variables.get(v))));
-		// for some reason, the extract invalidates the mlfa
-		isDirty = true;
+        for (Variable v : variableSet) {
+            System.out.println(variables.get(v));
+            a = a.union(m2a.extract(g2m.getMLFAStatePair(variables.get(v))));
+        }
+        // for some reason, the extract invalidates the mlfa
+        isDirty = true;
 
-		return a;
-	}
+        return a;
+    }
 
-	/**
-	 * Inserts the ID of a variable into the inserted set. If it is not in the
-	 * set it will be added and isDirty will be set to true.
-	 *
-	 * @param id The ID of the variable to be added to the set.
-	 * @return true if the variable was inserted; false if it already exists.
-	 */
-	private boolean insert(int id) {
-		boolean added = inserted.add(id);
-		if (added)
-			isDirty = true;
-		return added;
-	}
+    /**
+     * Inserts the ID of a variable into the inserted set. If it is not in the
+     * set it will be added and isDirty will be set to true.
+     *
+     * @param id The ID of the variable to be added to the set.
+     * @return true if the variable was inserted; false if it already exists.
+     */
+    private boolean insert(int id) {
+        boolean added = inserted.add(id);
+        if (added)
+            isDirty = true;
+        return added;
+    }
 
-	@Override
-	public VariableGrammar merge(VariableGrammar other) {
-		// all references of grammar are the same object
-		variables.putAll(other.variables);
-		inserted.addAll(inserted);
-		return this;
-	}
+    @Override
+    public VariableGrammar merge(VariableGrammar other) {
+        // all references of grammar are the same object
+//		variables.putAll(other.variables);
+//		inserted.addAll(other.inserted);
+//		visited.addAll(other.visited);
+        return this;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof VariableGrammar))
-			return false;
-		VariableGrammar other = (VariableGrammar) obj;
-		if (!variables.equals(other.variables))
-			return false;
-		return inserted.equals(other.inserted);
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof VariableGrammar))
+            return false;
+        VariableGrammar other = (VariableGrammar) obj;
+        if (!variables.equals(other.variables))
+            return false;
+        return inserted.equals(other.inserted);
+    }
 
-	@Override
-	public String toString() {
-		return grammar.toString();
-	}
+    @Override
+    public String toString() {
+        return grammar.toString();
+    }
 
-	@Override
-	public VariableGrammar clone() {
-		VariableGrammar clone = new VariableGrammar();
-		clone.grammar = grammar;
-		// TODO: change putAll and addAll
-		clone.variables.putAll(variables);
-		clone.inserted.addAll(inserted);
-		return clone;
-	}
+    @Override
+    public VariableGrammar clone() {
+        VariableGrammar clone = new VariableGrammar();
+        clone.grammar = grammar;
+        clone.visited = visited;
+        clone.variables = variables;
+        clone.inserted = inserted;
+        // TODO: change putAll and addAll
+//		clone.variables.putAll(variables);
+//		clone.inserted.addAll(inserted);
+//		clone.visited.addAll(visited);
+        return clone;
+    }
 
 }
