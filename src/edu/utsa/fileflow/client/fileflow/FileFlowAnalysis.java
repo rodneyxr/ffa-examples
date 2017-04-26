d9715ea589d9ad3dc906e49a94c96a1e26bb9266
Rodney Rodriguez <rodneyxr@gmail.com>
2017-04-26 16:26:30 -0500

    Integrated variable analysis and grammar analysis into file flow analysis

--- src/edu/utsa/fileflow/client/fileflow/FileFlowAnalysis.java
+++ src/edu/utsa/fileflow/client/fileflow/FileFlowAnalysis.java
@@ -2,192 +2,99 @@ package edu.utsa.fileflow.client.fileflow;
 
 import edu.utsa.fileflow.analysis.Analysis;
 import edu.utsa.fileflow.analysis.AnalysisException;
-import edu.utsa.fileflow.antlr.FileFlowParser.*;
+import edu.utsa.fileflow.antlr.FileFlowParser.FunctionCallContext;
+import edu.utsa.fileflow.antlr.FileFlowParser.ValueContext;
 import edu.utsa.fileflow.cfg.FlowPointContext;
+import edu.utsa.fileflow.client.AssignContext;
+import edu.utsa.fileflow.client.fileflow.grammar.GrammarAnalysisDomain;
+import edu.utsa.fileflow.client.fileflow.variable.VariableAnalysisDomain;
 import edu.utsa.fileflow.utilities.GraphvizGenerator;
-import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class FileFlowAnalysis extends Analysis<FileFlowAnalysisDomain> {
 
-    @Override
-    public FileFlowAnalysisDomain onFinish(FileFlowAnalysisDomain domain) throws AnalysisException {
-        // write automaton to DOT file
-        GraphvizGenerator.saveDOTToFile(domain.post.toDot(), "scripts/automaton.dot");
-        return domain;
-    }
+	private GrammarAnalysisDomain gDomain;
+	private VariableAnalysisDomain vDomain;
 
-    @Override
-    public FileFlowAnalysisDomain touch(FileFlowAnalysisDomain domain, FlowPointContext context)
-            throws AnalysisException {
-        VariableAutomaton va = getValue(domain, context, 0);
+	@Override
+	public FileFlowAnalysisDomain onFinish(FileFlowAnalysisDomain domain) throws AnalysisException {
+		// write automaton to DOT file
+		GraphvizGenerator.saveDOTToFile(domain.post.toDot(), "scripts/automaton.dot");
+		return domain;
+	}
 
-        // add the automaton to the file structure
-        try {
-            domain.post.createFile(va);
-        } catch (FileStructureException e) {
-            throw new AnalysisException(e.getMessage());
-        }
+	@Override
+	public FileFlowAnalysisDomain onBefore(FileFlowAnalysisDomain domain, FlowPointContext context) throws AnalysisException {
+		gDomain = (GrammarAnalysisDomain) context.getFlowPoint().getDomain(GrammarAnalysisDomain.class);
+		vDomain = (VariableAnalysisDomain) context.getFlowPoint().getDomain(VariableAnalysisDomain.class);
+		return super.onBefore(domain, context);
+	}
 
-        return domain;
-    }
+	@Override
+	public FileFlowAnalysisDomain touch(FileFlowAnalysisDomain domain, FlowPointContext context)
+			throws AnalysisException {
+		VariableAutomaton va = getValue(domain, context, 0);
 
-    @Override
-    public FileFlowAnalysisDomain mkdir(FileFlowAnalysisDomain domain, FlowPointContext context)
-            throws AnalysisException {
-        VariableAutomaton va = getValue(domain, context, 0);
+		// add the automaton to the file structure
+		try {
+			domain.post.createFile(va);
+		} catch (FileStructureException e) {
+			throw new AnalysisException(e.getMessage());
+		}
 
-        // add the automaton to the file structure
-        try {
-            domain.post.createDirectory(va);
-        } catch (FileStructureException e) {
-            throw new AnalysisException(e.getMessage());
-        }
+		return domain;
+	}
 
-        return domain;
-    }
+	@Override
+	public FileFlowAnalysisDomain mkdir(FileFlowAnalysisDomain domain, FlowPointContext context)
+			throws AnalysisException {
+		VariableAutomaton va = getValue(domain, context, 0);
 
-    @Override
-    public FileFlowAnalysisDomain copy(FileFlowAnalysisDomain domain, FlowPointContext context)
-            throws AnalysisException {
-        VariableAutomaton v1 = getValue(domain, context, 0);
-        VariableAutomaton v2 = getValue(domain, context, 1);
+		// add the automaton to the file structure
+		try {
+			domain.post.createDirectory(va);
+		} catch (FileStructureException e) {
+			throw new AnalysisException(e.getMessage());
+		}
 
-        try {
-            domain.post.copy(v1, v2);
-        } catch (FileStructureException e) {
-            throw new AnalysisException(e.getMessage());
-        }
+		return domain;
+	}
 
-        return domain;
-    }
+	@Override
+	public FileFlowAnalysisDomain copy(FileFlowAnalysisDomain domain, FlowPointContext context)
+			throws AnalysisException {
+		VariableAutomaton v1 = getValue(domain, context, 0);
+		VariableAutomaton v2 = getValue(domain, context, 1);
 
-    @Override
-    public FileFlowAnalysisDomain enterAssignment(FileFlowAnalysisDomain domain, FlowPointContext context)
-            throws AnalysisException {
-        // assignment
-        // : Variable Index '=' arrayValue
-        // | Variable '=' varValue
+		try {
+			domain.post.copy(v1, v2);
+		} catch (FileStructureException e) {
+			throw new AnalysisException(e.getMessage());
+		}
 
-        AssignmentContext ctx = (AssignmentContext) context.getContext();
-        VarValueContext var = ctx.varValue();
-        ArrayValueContext arr = ctx.arrayValue();
+		return domain;
+	}
 
-        // true if variable on left side is an array
-        boolean isArray = (ctx.Index() != null);
+	@Override
+	public FileFlowAnalysisDomain enterAssignment(FileFlowAnalysisDomain domain, FlowPointContext context) throws AnalysisException {
+		AssignContext ctx = new AssignContext(context);
+		domain.table.put(ctx.var0, new VariableAutomaton(gDomain.getVariable(ctx.var0, vDomain.getLiveVariables())));
+		// TODO: handle arrays and user INPUT
+		return super.enterAssignment(domain, context);
+	}
 
-        ExpressionContext expr = null;
-        TerminalNode input = null;
-        TerminalNode emptyArray = null;
+	/**
+	 * Gets the nth parameter of a function call.
+	 */
+	private VariableAutomaton getValue(FileFlowAnalysisDomain domain, FlowPointContext context, int n) {
+		FunctionCallContext ctx = (FunctionCallContext) context.getContext();
+		ValueContext v = ctx.value(n);
 
-        // if assignment variable is array and has index
-        if (isArray) {
-            if (arr.varValue() == null) {
-                emptyArray = arr.EmptyValue();
-            } else {
-                expr = arr.varValue().expression();
-                input = arr.varValue().Input();
-            }
-        } else {
-            // if assignment is a regular variable with no index
-            expr = var.expression();
-            input = var.Input();
-        }
+		// get the variable from the symbol table or create a new one
+		if (v.Variable() != null) // if v is a variable
+			return domain.table.get(v.Variable().getText());
 
-        // variable name (key to update)
-        String key = ctx.Variable().getText();
-        if (isArray)
-            key = key + ctx.Index().getText();
-
-        VariableAutomaton t1 = null;
-        VariableAutomaton t2 = null;
-
-        // if array variable is assigned to empty array
-        if (emptyArray != null) {
-            t1 = VariableAutomaton.bottom();
-            VariableAutomaton old = domain.table.get(key);
-            if (old != null)
-                domain.table.put(key, t1.concatenate(old));
-            else
-                domain.table.put(key, t1);
-            return domain;
-        }
-
-        // if user input is required
-        if (input != null) {
-            // INPUT is equivalent to '*'
-            VariableAutomaton val = VariableAutomaton.top();
-            domain.table.put(key, val);
-            return domain;
-        }
-
-        // get the first term
-        ValueContext val = expr.value(0);
-        TerminalNode term1 = val.Variable();
-        if (term1 != null) {
-            // term1 is a variable
-            String term1Text = term1.getText();
-            if (val.Index() != null)
-                term1Text += val.Index().getText();
-
-            t1 = domain.table.get(term1Text);
-            if (t1 == null)
-                throw new AnalysisException("variable '" + term1Text + "' not defined");
-
-        } else {
-            // term1 is a string
-            term1 = val.String();
-            t1 = new VariableAutomaton(term1.getText());
-        }
-
-        // check for concatenation
-        if (expr.value().size() == 2) {
-            val = expr.value(1);
-            TerminalNode term2 = val.Variable();
-
-            if (term2 != null) {
-                // term2 is a variable
-                String term2Text = term2.getText();
-                if (val.Index() != null)
-                    term2Text += val.Index().getText();
-
-                t2 = domain.table.get(term2Text);
-                if (t2 == null)
-                    throw new AnalysisException("variable '" + term2Text + "' not defined");
-
-            } else {
-                // term2 is a string
-                term2 = val.String();
-                t2 = new VariableAutomaton(term2.getText());
-            }
-
-            // concatenate t1 and t2
-            VariableAutomaton old = domain.table.get(key);
-            if (old != null && isArray)
-                domain.table.put(key, t1.concatenate(t2).merge(old));
-            else
-                domain.table.put(key, t1.concatenate(t2));
-
-        } else {
-            // there is no concatenation, so just add t1 to symbol table
-            VariableAutomaton old = domain.table.get(key);
-            if (old != null && isArray)
-                t1.merge(old);
-            domain.table.put(key, t1);
-        }
-
-        return domain;
-    }
-
-    private VariableAutomaton getValue(FileFlowAnalysisDomain domain, FlowPointContext context, int n) {
-        FunctionCallContext ctx = (FunctionCallContext) context.getContext();
-        ValueContext v = ctx.value(n);
-
-        // get the variable from the symbol table or create a new one
-        if (v.Variable() != null) // if v is a variable
-            return domain.table.get(v.Variable().getText());
-
-        // v is a string literal
-        return new VariableAutomaton(v.String().getText());
-    }
+		// v is a string literal
+		return new VariableAutomaton(v.String().getText());
+	}
 
 }
---
+++ test/edu/utsa/fileflow/client/fileflow/tools/FFADriver.java
@@ -0,0 +1,83 @@
+package edu.utsa.fileflow.client.fileflow.tools;
+
+import edu.utsa.fileflow.analysis.AnalysisException;
+import edu.utsa.fileflow.analysis.Analyzer;
+import edu.utsa.fileflow.cfg.FlowPoint;
+import edu.utsa.fileflow.client.fileflow.FileFlowAnalysis;
+import edu.utsa.fileflow.client.fileflow.FileFlowAnalysisDomain;
+import edu.utsa.fileflow.client.fileflow.grammar.GrammarAnalysis;
+import edu.utsa.fileflow.client.fileflow.grammar.GrammarAnalysisDomain;
+import edu.utsa.fileflow.client.fileflow.variable.LiveVariableMap;
+import edu.utsa.fileflow.client.fileflow.variable.VariableAnalysis;
+import edu.utsa.fileflow.client.fileflow.variable.VariableAnalysisDomain;
+import edu.utsa.fileflow.utilities.FileFlowHelper;
+
+import java.io.IOException;
+
+/**
+ * This is a helper class to make it simple to run a script and store the results
+ * during testing.
+ * <p>
+ * Created by Rodney on 4/26/2017.
+ */
+public class FFADriver {
+
+	/* Variable Analysis */
+	public VariableAnalysisDomain variableAnalysisDomain;
+	public VariableAnalysis variableAnalysis;
+	public Analyzer<VariableAnalysisDomain, VariableAnalysis> variableAnalyzer;
+
+	/* Grammar Analysis */
+	public GrammarAnalysisDomain grammarDomain;
+	public GrammarAnalysis grammarAnalysis;
+	public Analyzer<GrammarAnalysisDomain, GrammarAnalysis> grammarAnalyzer;
+
+	/* File Flow Analysis */
+	public FileFlowAnalysisDomain ffaDomain;
+	public FileFlowAnalysis ffaAnalysis;
+	public Analyzer<FileFlowAnalysisDomain, FileFlowAnalysis> ffaAnalyzer;
+
+	/* Results */
+	public VariableAnalysisDomain variableResult;
+	public GrammarAnalysisDomain grammarResult;
+	public FileFlowAnalysisDomain ffaResult;
+	public LiveVariableMap liveVariables;
+
+	private FlowPoint cfg;
+
+	private FFADriver(String script) throws IOException {
+		cfg = FileFlowHelper.generateControlFlowGraphFromScript(script);
+	}
+
+	public static FFADriver run(String script) throws IOException, AnalysisException {
+		FFADriver ffaDriver = new FFADriver(script);
+		ffaDriver.setUp();
+		ffaDriver.createAnalyzers();
+		return ffaDriver;
+	}
+
+	private void setUp() {
+		/* Variable Analysis */
+		variableAnalysisDomain = new VariableAnalysisDomain();
+		variableAnalysis = new VariableAnalysis();
+		variableAnalyzer = new Analyzer<>(variableAnalysisDomain, variableAnalysis);
+
+		/* Grammar Analysis */
+		grammarDomain = new GrammarAnalysisDomain();
+		grammarAnalysis = new GrammarAnalysis();
+		grammarAnalyzer = new Analyzer<>(grammarDomain, grammarAnalysis);
+
+		/* File Flow Analysis */
+		ffaDomain = new FileFlowAnalysisDomain();
+		ffaAnalysis = new FileFlowAnalysis();
+		ffaAnalyzer = new Analyzer<>(ffaDomain, ffaAnalysis);
+	}
+
+	private void createAnalyzers() throws AnalysisException {
+		variableResult = variableAnalyzer.analyze(cfg);
+		grammarResult = grammarAnalyzer.analyze(cfg);
+		ffaResult = ffaAnalyzer.analyze(cfg);
+		liveVariables = variableResult.getLiveVariables();
+	}
+
+}
